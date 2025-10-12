bbb399c2ea73dabc20a166d5bdbf8a31ee4d146b
Rian <redkrsc@gmail.com>
2025-10-12 14:11:23 -0400

    Added key and working on Bunraku

--- Scripts/bunraku.gd
+++ Scripts/bunraku.gd
@@ -4,6 +4,9 @@ extends AnimatableBody3D
 
 @export var anger_headshake_factor: float
 @export var anger_bodyshake_factor: float
+@export var anger_decrease_delta: float
+@export var anger_decrease_max: float
+
 @export_range(0,1) var anger_level: float
 @onready var body_sprite: Sprite3D = $Body
 @onready var head_sprite: Sprite3D = $Body/Head
@@ -16,6 +19,11 @@ func _ready() -> void:
 	mat = head_sprite.material_override as StandardMaterial3D
 	
 func _physics_process(_delta: float) -> void:
+	anger_decrease_delta += _delta
+	anger_decrease_delta = clamp(anger_decrease_delta, -_delta*2, anger_decrease_max)
+	anger_level -= anger_decrease_delta * _delta
+	anger_level = clamp(anger_level, 0, 1)
+	
 	if not active: return
 	body_sprite.offset = Vector2.ZERO
 	head_sprite.rotation.z = 0
--- Scripts/yono.gd
+++ Scripts/yono.gd
@@ -8,10 +8,22 @@ extends Bunraku
 @export var too_close_curve: Curve
 @export var too_close_factor: float
 
+func _ready() -> void:
+	super._ready()
+	active = true
+
 func _physics_process(_delta: float) -> void:
-	if Engine.is_editor_hint(): return
+	debug_draw(_delta)
+	if Engine.is_editor_hint(): 
+		
+		return
 	var _dist_to_player = (Player.instance.global_position - global_position).length()
 	if _dist_to_player < too_close_distance:
-		var _samp = too_close_curve.sample(1-(_dist_to_player/too_close_factor))
+		var _samp = too_close_curve.sample(1-(_dist_to_player/too_close_distance))
 		anger_level += _delta * _samp * too_close_factor
+		anger_decrease_delta = 0
 	super._physics_process(_delta)
+
+func debug_draw(_delta: float) -> void:
+	var _a11 = DebugDraw3D.new_scoped_config().set_thickness(0.005)
+	DebugDraw3D.draw_sphere(global_position, too_close_distance, Color.RED)
